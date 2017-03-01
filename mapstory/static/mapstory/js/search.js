77d681313e11865622ac8ea87089cbe68417d275
emilyashley <ohemilyashley@gmail.com>
2017-03-01 15:23:42 -0600

    removal of search function granularity, moving towards multiple_choice_listener

--- mapstory/static/mapstory/js/search.js
+++ mapstory/static/mapstory/js/search.js
@@ -58,7 +58,7 @@
     $scope.trending = [];
     $http.get(KEYWORDS_ENDPOINT, {}).success(function(data){ 
       $scope.keywords = data.objects
-
+      //poor choice in sorting
       var results = data.objects.sort(function(kw1, kw2) {
         return kw2.count - kw1.count;
       });
@@ -108,8 +108,6 @@
       $scope.search();
     };
 
-    
-
 
     $scope.django = Django.all();
 
@@ -143,6 +141,24 @@
       });
     };
 
+    /*
+    * ADD TYPE MAPSTORY OR LAYER
+    */
+    $scope.contentType = function(type) {
+      $scope.query['type__in'] = type;
+      query_api($scope.query);
+    }
+
+    /*
+    * Toggle adding/removing this filter
+    */
+    $scope.toggle_query = function(toggle, filter, value) {
+      if (toggle) {
+        add_query(filter, value);
+      } else {
+        remove_query(filter, value);
+      }
+    }
 
     /*
     * Pagination 
@@ -188,36 +204,8 @@
 
 
 
-
-    //used in detail page 'related' tab and featured content on index page
-    $scope.query_category = function(category, type) {
-      $scope.query.type__in = type; //only needed for detail page
-      $scope.query.category__identifier__in = category;
-      $scope.search();
-    };
-
-    /*
-    * Setting the query to a single element - replaces single_choice_listener
-    */
-    $scope.set_query = function(filter, value) {
-      $scope.query = {};
-      $scope.query[filter] = value;
-      query_api($scope.query);
-    }
-
-    /*
-    * Add the query, replacing any current query
-    * Good for mutually exclusive types
-    */
-    $scope.add_single_query = function(filter, value) {
-      $scope.query[filter] = value;
-      query_api($scope.query);
-    }
-
-    /*
-    * Add the query, appending it to any current query
-    */
-    $scope.add_query = function(filter, value) {
+    /* functionality for tagging and queries */     
+    function add_query(filter, value) {
       var query_entry = [];
       if ($scope.query.hasOwnProperty(filter)) {
         //if theres a list of items, grab them. otherwise, add the only value to empty list
@@ -236,23 +224,16 @@
       }
       $scope.query[filter] = query_entry;
       query_api($scope.query);
-    }
+    };
 
-    /*
-    * Toggle adding/removing this filter
-    */
-    $scope.toggle_query = function(toggle, filter, value) {
-      if (toggle) {
-        $scope.add_query(filter, value);
-      } else {
-        $scope.remove_query(filter, value);
+    $scope.add_search = function(filter, value, array) {
+      if (array.indexOf(value) == -1) {
+        array.push(value);
+        add_query(filter, value);
       }
     }
 
-    /*
-    * Remove the query
-    */
-    $scope.remove_query = function(filter, value) {
+    function remove_query(filter, value) {
       var query_entry = [];
       // First check if this even exists to remove
       if ($scope.query.hasOwnProperty(filter)) {
@@ -268,174 +249,143 @@
         $scope.query[filter] = query_entry;
         query_api($scope.query);
       }
-    }
-
-    /*
-    * Delete all parts of this filter
-    */
-    $scope.delete_query = function(filter) {
-      // First check if this even exists to remove
-      if ($scope.query.hasOwnProperty(filter)) {
-        $scope.query[filter] = [];
-        query_api($scope.query);
-      }
-    }
-
-    $scope.add_search = function(filter, value, array) {
-      if (array.indexOf(value) == -1) {
-        array.push(value);
-        $scope.add_query(filter, value);
-      }
-    }
+    };
 
     $scope.remove_search = function(filter, value, array) {
       var index = array.indexOf(value);
       if (index != -1) {
         array.splice(index, 1);
-        $scope.remove_query(filter, value);
+        remove_query(filter, value);
       }
     }
 
-    $scope.remove_all_queries = function(){
-      console.log($scope.query);
-      query_api($scope.query);
+    // Configure new autocomplete
+    var profile_autocompletes = [];
+    var region_autocompletes = [];
+    var keyword_autocompletes = [];
+    var city_autocompletes = [];
+
+
+    var usernames = [];
+    var regions = [];
+    var keywords = [];
+    var cities = [];
+    var countries = [];
+    // This will contain the country code, i.e. Canada's code is CAN, at the same index location
+    // as region_autocompletes stores the country name.
+    var country_codes = [];
+
+    function init_tokenfields() {
+      var deferred = $q.defer();
+      var promises = [];
+      promises.push(
+        profile_autocomplete()
+        .then(function() {
+          $('#tokenfield-profile')
+            .tokenfield({
+              autocomplete: {
+                source: profile_autocompletes,
+                delay: 100,
+                minLength: 3
+              },
+              showAutocompleteOnFocus: true,
+              limit: 10
+            })
+            .on('tokenfield:createdtoken', function(e) {
+               $scope.add_search('owner__username__in', e.attrs.value, usernames);
+            })
+            .on('tokenfield:removedtoken', function(e) {
+              $scope.remove_search('owner__username__in', e.attrs.value, usernames);
+            });
+
+          $('#tokenfield-city')
+            .tokenfield({
+              autocomplete: {
+                source: city_autocompletes
+              },
+              limit: 10
+            })
+            .on('tokenfield:createdtoken', function(e) {
+              $scope.add_search('city', e.attrs.value, cities);
+            })
+            .on('tokenfield:removedtoken', function(e) {
+              $scope.remove_search('city', e.attrs.value, cities);
+            });
+        })
+      );
+
+      promises.push(
+        keyword_autocomplete()
+        .then(function() {
+          $('#tokenfield-keyword')
+          .tokenfield({
+            autocomplete: {
+              source: keyword_autocompletes,
+              delay: 100,
+              minLength: 3
+            },
+            showAutocompleteOnFocus: true,
+            limit: 10
+          })
+          .on('tokenfield:createtoken', function(e) {
+            // Tokenize by space if num_spaces > 3
+            var num_spaces = (e.attrs.value.match(/ /g)||[]).length;
+            var data = e.attrs.value.split(' ');
+            if (num_spaces > 3) {
+              e.attrs.value = data[0];
+              e.attrs.label = data[0];
+              for (var i = 1; i < data.length; i++) {
+                $('#tokenfield-keyword').tokenfield('createToken', data[i]);
+              }
+            }
+          })
+          .on('tokenfield:createdtoken', function(e) {
+            $scope.add_search('keywords__slug__in', e.attrs.value, keywords);
+          })
+          .on('tokenfield:removedtoken', function(e) {
+            $scope.remove_search('keywords__slug__in', e.attrs.value, keywords);
+          });
+        })
+      );
+
+      $q.all(promises)
+        .then(function() {
+          deferred.resolve();
+        }, function() {
+          deferred.reject('Some tokenization field initializations failed');
+        }
+      );
+      return deferred.promise;
     }
 
-    // front-end fix until we update or create an api that doesn't include maps
-    $scope.all_types = function(){
-      $scope.delete_query('type__in');
-      $scope.add_query('type__in', 'mapstory');
-      $scope.add_query('type__in', 'layer');
-      query_api($scope.query);
-    }
-
-// Configure new autocomplete
-var profile_autocompletes = [];
-var region_autocompletes = [];
-var keyword_autocompletes = [];
-var city_autocompletes = [];
-
-
-var usernames = [];
-var regions = [];
-var keywords = [];
-var cities = [];
-var countries = [];
-// This will contain the country code, i.e. Canada's code is CAN, at the same index location
-// as region_autocompletes stores the country name.
-var country_codes = [];
-
-function init_tokenfields() {
-  var deferred = $q.defer();
-  var promises = [];
-  promises.push(
-    profile_autocomplete()
-    .then(function() {
-      $('#tokenfield-profile')
-        .tokenfield({
-          autocomplete: {
-            source: profile_autocompletes,
-            delay: 100,
-            minLength: 3
-          },
-          showAutocompleteOnFocus: true,
-          limit: 10
-        })
-        .on('tokenfield:createdtoken', function(e) {
-           $scope.add_search('owner__username__in', e.attrs.value, usernames);
-        })
-        .on('tokenfield:removedtoken', function(e) {
-          $scope.remove_search('owner__username__in', e.attrs.value, usernames);
-        });
-
-      $('#tokenfield-city')
-        .tokenfield({
-          autocomplete: {
-            source: city_autocompletes
-          },
-          limit: 10
-        })
-        .on('tokenfield:createdtoken', function(e) {
-          $scope.add_search('city', e.attrs.value, cities);
-        })
-        .on('tokenfield:removedtoken', function(e) {
-          $scope.remove_search('city', e.attrs.value, cities);
-        });
-    })
-  );
-
-  promises.push(
-    keyword_autocomplete()
-    .then(function() {
-      $('#tokenfield-keyword')
-      .tokenfield({
-        autocomplete: {
-          source: keyword_autocompletes,
-          delay: 100,
-          minLength: 3
-        },
-        showAutocompleteOnFocus: true,
-        limit: 10
-      })
-      .on('tokenfield:createtoken', function(e) {
-        // Tokenize by space if num_spaces > 3
-        var num_spaces = (e.attrs.value.match(/ /g)||[]).length;
-        var data = e.attrs.value.split(' ');
-        if (num_spaces > 3) {
-          e.attrs.value = data[0];
-          e.attrs.label = data[0];
-          for (var i = 1; i < data.length; i++) {
-            $('#tokenfield-keyword').tokenfield('createToken', data[i]);
+    function profile_autocomplete() {
+      return $http.get('/api/owners/')
+        .success(function(data){
+          var results = data.objects;
+          // Here we have first name, last name, and username
+          // append them all together to be used in the profile autocomplete
+          for (var i = 0; i < results.length; i++) {
+            profile_autocompletes.push(results[i].first_name);
+            profile_autocompletes.push(results[i].last_name);
+            profile_autocompletes.push(results[i].username);
+            if (results[i].city != null) {
+              city_autocompletes.push(results[i].city);
+            }
           }
-        }
-      })
-      .on('tokenfield:createdtoken', function(e) {
-        $scope.add_search('keywords__slug__in', e.attrs.value, keywords);
-      })
-      .on('tokenfield:removedtoken', function(e) {
-        $scope.remove_search('keywords__slug__in', e.attrs.value, keywords);
-      });
-    })
-  );
+        });
+    };
 
-  $q.all(promises)
-    .then(function() {
-      deferred.resolve();
-    }, function() {
-      deferred.reject('Some tokenization field initializations failed');
-    }
-  );
-  return deferred.promise;
-}
+    function keyword_autocomplete() {
+      return $http.get('/api/keywords/')
+        .success(function(data){
+          var results = data.objects;
+          for (var i = 0; i < results.length; i++) {
+            keyword_autocompletes.push(results[i].slug);
+          }
+        });
+    };
 
-function profile_autocomplete() {
-  return $http.get('/api/owners/')
-    .success(function(data){
-      var results = data.objects;
-      // Here we have first name, last name, and username
-      // append them all together to be used in the profile autocomplete
-      for (var i = 0; i < results.length; i++) {
-        profile_autocompletes.push(results[i].first_name);
-        profile_autocompletes.push(results[i].last_name);
-        profile_autocompletes.push(results[i].username);
-        if (results[i].city != null) {
-          city_autocompletes.push(results[i].city);
-        }
-      }
-    });
-};
-
-function keyword_autocomplete() {
-  return $http.get('/api/keywords/')
-    .success(function(data){
-      var results = data.objects;
-      for (var i = 0; i < results.length; i++) {
-        keyword_autocompletes.push(results[i].slug);
-      }
-    });
-};
-
-init_tokenfields()
+    init_tokenfields()
 
     $scope.filterVTC = function() {
       // When VTC check box is clicked, also filter by VTC; when unchecked, reset it
