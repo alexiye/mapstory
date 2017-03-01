63affc015b0683ed33ddccc7aa52a6e8e4fe8c18
emilyashley <ohemilyashley@gmail.com>
2017-03-01 15:38:57 -0600

    multiple_choice_listener init

--- mapstory/static/mapstory/js/search.js
+++ mapstory/static/mapstory/js/search.js
@@ -160,6 +160,64 @@
       }
     }
 
+    
+    /*
+    * Add the selection behavior to the element, it adds/removes the 'active' class
+    * and pushes/removes the value of the element from the query object
+    */
+    $scope.multiple_choice_listener = function($event){
+      var element = $($event.target);
+      var query_entry = [];
+      var data_filter = element.attr('data-filter');
+      var value = element.attr('data-value');
+
+      // If the query object has the record then grab it
+      if ($scope.query.hasOwnProperty(data_filter)){
+
+        // When in the location are passed two filters of the same
+        // type then they are put in an array otherwise is a single string
+        if ($scope.query[data_filter] instanceof Array){
+          query_entry = $scope.query[data_filter];
+        }else{
+          query_entry.push($scope.query[data_filter]);
+        }
+      }
+
+      // If the element is active active then deactivate it
+      if(element.hasClass('active')){
+        // clear the active class from it
+        element.removeClass('active');
+
+        // Remove the entry from the correct query in scope
+        query_entry.splice(query_entry.indexOf(value), 1);
+      }
+      // if is not active then activate it
+      else if(!element.hasClass('active')){
+        // Add the entry in the correct query
+        if (query_entry.indexOf(value) == -1){
+          query_entry.push(value);
+        }
+        element.addClass('active');
+      }
+
+      //save back the new query entry to the scope query
+      $scope.query[data_filter] = query_entry;
+
+      //if the entry is empty then delete the property from the query
+      if(query_entry.length == 0){
+        delete($scope.query[data_filter]);
+      }
+      query_api($scope.query);
+    }
+
+
+
+
+
+
+
+
+
     /*
     * Pagination 
     */
